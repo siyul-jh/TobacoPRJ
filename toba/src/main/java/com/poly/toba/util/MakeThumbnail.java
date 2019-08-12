a623b777b2a1305eebeed4a65c454710723eeea9
leejiheum <gihum@naver.com>
2019-08-12 13:35:01 +0900

    Image resize

--- toba/src/main/java/com/poly/toba/util/MakeThumbnail.java
+++ toba/src/main/java/com/poly/toba/util/MakeThumbnail.java
@@ -2,6 +2,7 @@ package com.poly.toba.util;
 
 import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
@@ -11,8 +12,6 @@ import java.util.UUID;
 
 import javax.imageio.ImageIO;
 import javax.xml.bind.DatatypeConverter;
-
-import org.imgscalr.Scalr;
 
 public class MakeThumbnail {
 	private static final int IMG_WIDTH = 1920;
@@ -87,43 +86,51 @@ public class MakeThumbnail {
 		return resizedImage;
 	}
 	// 광고 이미지 리사이징
-		public static String adImgResize(String datatype,String data, String now) throws Exception{
-			String succes = "";
-			byte[] imageBytes = DatatypeConverter.parseBase64Binary(data);
-		    String year = now.substring(0,4);
-			String month = now.substring(4,6);
-			String day = now.substring(6,8);
-			UUID uid = UUID.randomUUID();
-			
-			String srcAdImg = "http://15.164.160.236:8080/imageUpload/ad/"+year+"/"+month+"/"+day+"/Thum_";
-			String path = "/usr/local/tomcat/webapps/ROOT/imageUpload/ad/"+year+"/"+month+"/"+day;
-			
-			String fileName = uid + now+"."+datatype;
+	public static String adImgResize(String datatype,String data, String now) throws Exception{
+		String succes = "";
+		byte[] imageBytes = DatatypeConverter.parseBase64Binary(data);
+	    String year = now.substring(0,4);
+		String month = now.substring(4,6);
+		String day = now.substring(6,8);
+		UUID uid = UUID.randomUUID();
+		
+		String srcAdImg = "http://15.164.160.236:8080/imageUpload/ad/"+year+"/"+month+"/"+day+"/Thum_";
+		String path = "/usr/local/tomcat/webapps/ROOT/imageUpload/ad/"+year+"/"+month+"/"+day;
+		
+		String fileName = uid + now+"."+datatype;
+		
+		int width = 828;
+		int height = 150;
 
-			try {
+		try {
 				File filePath = new File(path);
 		    	if(!filePath.exists()){ //파일존재여부확인
 		    		filePath.mkdirs();
-		        }
-				BufferedImage srcImg = ImageIO.read(new ByteArrayInputStream(imageBytes));
-
-				// 원본이미지를 높이 160px로 수정 
-				BufferedImage destImg = Scalr.resize(srcImg, Scalr.Method.AUTOMATIC, Scalr.Mode.FIT_TO_HEIGHT, 60);
-				// 썸네일을 저장합니다. 이미지 이름 앞에 "Thum_" 표시 
-				String thumName = path + "/Thum_" + fileName;
-				// 파일 생성
-				try {
-					File thumbFile = new File(thumName);
-					ImageIO.write(destImg, datatype, thumbFile);
-					succes = srcAdImg+fileName;
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+		    	}
+	    	//
+			BufferedImage srcImg = ImageIO.read(new ByteArrayInputStream(imageBytes));
+	    	// 이미지 리사이즈
+            // Image.SCALE_SMOOTH : 이미지 부드러움을 우선
+            Image resizeImage = srcImg.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+			BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+			Graphics2D graphics2D = resizedImage.createGraphics();
+			graphics2D.drawImage(resizeImage, 0, 0, width, height, null);
+			graphics2D.dispose();
+			// 썸네일을 저장합니다. 이미지 이름 앞에 "Thum_" 표시 
+			String thumName = path + "/Thum_" + fileName;
+			// 파일 생성
+			try {
+				File thumbFile = new File(thumName);
+				ImageIO.write(resizedImage, datatype, thumbFile);
+				succes = srcAdImg+fileName;
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			return succes;
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		return succes;
+	}
 	public static String makeThumnailProfile(String path, String newFileName, String thumbFileName, String extension,
 											String year, String month, String day, String hour, String userNo) throws Exception {
 		BufferedImage originalImage = ImageIO.read(new File(newFileName));
